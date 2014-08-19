3dce5cc27c16d4155e157fad119ad2069eed46d7
LeSanglier <lesanglierdesardennes@gmail.com>
2014-08-19 21:44:52 +0200

    Compute delta

--- horloge.py
+++ horloge.py
@@ -4,6 +4,7 @@
 import os, sys, pygame
 from pygame.locals import *
 import datetime
+from subprocess import *
 
 def main():
     size = width, height = 600, 300
@@ -21,6 +22,11 @@ def main():
     white = 255,255,255
     font = pygame.font.Font("font.ttf", 128)
 
+    #commandeDate = ["date", "+%Y-%m-%d %H:%M:%S.%N"]
+    commandeDate = ["date", "+%S.%N"]
+
+    delta = datetime.timedelta(seconds = 0)
+
     while 1:
 
         for event in pygame.event.get():
@@ -30,16 +36,22 @@ def main():
         screen.fill(black)
 
         now = datetime.datetime.today()
-        delta = datetime.timedelta(seconds = 2)
+        secPython = float(str(now)[17:26])
 
         dt = str(now - delta)
         time = dt[11:19]
-        
+
         fontimg = font.render(time, 1, white)
         screen.blit(fontimg, (50,80))
 
         pygame.display.update() 
         pygame.time.delay(1000)
         
+        # Compute delta
+        out=Popen(commandeDate,stdout=PIPE)
+        (secUnix,serr)=out.communicate()
+        delta = datetime.timedelta(seconds = int(float(secUnix) - float(secPython)))
+
+
 if __name__ == '__main__': 
     main()    
